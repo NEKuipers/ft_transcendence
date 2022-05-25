e435ed31d0aa5ab30bfa58325d2f93b9275f733f
Nick Kuipers <nickkuipers92@gmail.com>
2022-05-25 11:39:47 +0200

    Initial upload of basic frontend/backend scaffolding

---
+++ backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ServeStaticModule} from '@nestjs/serve-static'; // New
+import { join } from 'path'; // New
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({ // New
+      rootPath: join(__dirname, '..', '../frontend/dist'), // New
+    }), // New
+  ],
+ controllers: [AppController],
+ providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('backend'); // New
+  await app.listen(3000);
+}
+bootstrap();
