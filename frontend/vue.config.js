2b35ed71fefaa62fb046c6b6a08b659c64a05d1d
Turlough Mullan <duke1199sp@gmail.com>
2022-06-07 14:48:20 +0200

    Added a proxy to wherever we are going to host the backedn

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+// Proxy to wherever we are going to host the backend
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/api': '/'},
+      }
+    }
+  }
+}
