1cbe728c074bb6f65790061136f44f3dbba5d87b
Turlough Mullan <duke1199sp@gmail.com>
2022-06-07 15:44:05 +0200

    Added a basic login button. Still requires CSS tweaking

---
+++ frontend/app/src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+	<button @click="onClick()" class="btn"> {{text}} </button>
+</template>
+	
+<script lang="ts">
+
+export default {
+	name: 'MyButton',
+	props: {
+		text: String,
+		color: String
+	},
+	methods: {
+		onClick() { console.log("Cliccato diobe") }
+	}
+}
+</script>
+
+<!-- <style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style> -->
--- frontend/app/src/views/LoginView.vue
+++ frontend/app/src/views/LoginView.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="login">
     <h1>Login page goes here</h1>
+    <header>
+      <MyButton text='Login Here'/>
+    </header>
   </div>
 </template>
+
+<script lang="ts">
+import MyButton from '../components/Button.vue'
+
+export default {
+  name: 'LoginView',
+  props: {
+
+  },
+  components: {
+    MyButton
+  }
+}
+</script>
+
+<style scoped>
+    .btn {
+        display: inline-block;
+        background-color: #2c3e50;
+        border-radius: 10px;
+        border: 4px double #cccccc;
+        color: #eeeeee;
+        text-align: center;
+        font-size: 28px;
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        padding: 20px;
+        width: 200px;
+        -webkit-transition: all 0.5s;
+        -moz-transition: all 0.5s;
+        -o-transition: all 0.5s;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: 5px;
+    }
+</style>
