c9d04be958b8da98fd334199c67f678a031fab18
Nick Kuipers <nickkuipers92@gmail.com>
2022-06-08 19:04:35 +0200

    Edit: ProfileView now fetches a single profile instead of an array

--- frontend/app/src/views/ProfileView.vue
+++ frontend/app/src/views/ProfileView.vue
@@ -3,21 +3,21 @@
     <img class="profilePicture" src="../assets/Profile-picture-default.png">
     <br>
     <section class="names">
-      <h1 v-if="users">{{users[0].userName}}</h1>
+      <h1 v-if="user">{{user.userName}}</h1>
       <h1 v-else>Loading...</h1>
-      <h3 v-if="users">{{users[0].firstName}} {{users[0].lastName}}</h3>
+      <h3 v-if="user">{{user.firstName}} {{user.lastName}}</h3>
       <h3 v-else>Loading...</h3>
     </section>
     <section class="game-stats">
-      <div v-if="users">
-        <h4>Games played: {{users[0].gamesPlayed}}</h4>
-        <h4>Games won: {{users[0].gamesWon}}</h4>
-        <h4>Games lost: {{users[0].gamesLost}}</h4>
+      <div v-if="user">
+        <h4>Games played: {{user.gamesPlayed}}</h4>
+        <h4>Games won: {{user.gamesWon}}</h4>
+        <h4>Games lost: {{user.gamesLost}}</h4>
       </div>
       <div v-else>
-        <h4>Games played: 0</h4>
-        <h4>Games won: 0</h4>
-        <h4>Games lost: 0</h4>
+        <h4>Games played: </h4>
+        <h4>Games won: </h4>
+        <h4>Games lost: </h4>
       </div>
     </section>
   </div>
@@ -49,22 +49,21 @@ export default defineComponent({
   data () {
     return {
       selectedFile: null,
-      users: null,
+      user: null,
     }
   },
   mounted() {
-    fetch('http://localhost:3000/users')
+    let id = '1'; // This variable directs you to various profiles
+    fetch('http://localhost:3000/users/' + id)
     .then(res => res.json())
-    .then(data => this.users = data)
+    .then(data => this.user = data)
     .catch(err => console.log(err));
   },
   components: {
     // SmallButton,
   },
   methods: {
-    logUsers() {
-      console.log(this.users);
-    }
+
   },
 });
 
