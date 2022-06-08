78538ab96abbf4760941ba8917322a8fcc5f80e4
Nick Kuipers <nickkuipers92@gmail.com>
2022-06-08 17:08:50 +0200

    Feature: Very simple profile page exists now. Its contents are based on data fetched from http://localhost:3000/

--- frontend/app/src/views/ProfileView.vue
+++ frontend/app/src/views/ProfileView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="profile">
-  <div>
     <img class="profilePicture" src="../assets/Profile-picture-default.png">
     <br>
-    <SmallButton @btn-click="$emit('change-avatar')"
-      :text="'Change Avatar'"></SmallButton>
-    <h2>Username</h2>
-      <SmallButton @btn-click="$emit('change-username')"
-      :text="'Change Username'"></SmallButton>
-    <h3>FirstName LastName</h3>
-    <h4>Games played: 0</h4>
-    <h4>Games won: 0</h4>
-    <h4>Games lost: 0</h4>
-  </div>
+    <section class="names">
+      <h1 v-if="users">{{users[0].userName}}</h1>
+      <h1 v-else>Loading...</h1>
+      <h3 v-if="users">{{users[0].firstName}} {{users[0].lastName}}</h3>
+      <h3 v-else>Loading...</h3>
+    </section>
+    <section class="game-stats">
+      <div v-if="users">
+        <h4>Games played: {{users[0].gamesPlayed}}</h4>
+        <h4>Games won: {{users[0].gamesWon}}</h4>
+        <h4>Games lost: {{users[0].gamesLost}}</h4>
+      </div>
+      <div v-else>
+        <h4>Games played: 0</h4>
+        <h4>Games won: 0</h4>
+        <h4>Games lost: 0</h4>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -33,12 +40,50 @@
 <script lang="ts">
 
 import { defineComponent } from 'vue';
-import SmallButton from '../components/SmallButton.vue';
+
 
 export default defineComponent({
   name: 'ProfileView',
+  props: {
+    },
+  data () {
+    return {
+      selectedFile: null,
+      users: null,
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/users')
+    .then(res => res.json())
+    .then(data => this.users = data)
+    .catch(err => console.log(err));
+  },
   components: {
-    SmallButton,
+    // SmallButton,
+  },
+  methods: {
+    logUsers() {
+      console.log(this.users);
+    }
   },
 });
+
 </script>
+
+<style scoped>
+
+.names {
+  margin-top: 0px;
+}
+
+.game-stats {
+  padding: 0px;
+  margin: 0px;
+}
+
+.game-stats h4 {
+  padding: 0px;
+  margin: 5px;
+}
+
+</style>
