1b114f78989fbb630f12bf6ab7a6eab4efacbab3
Nick Kuipers <nickkuipers92@gmail.com>
2022-06-10 15:10:14 +0200

    minor issue fix

--- frontend/app/src/components/EditUserProfile.vue
+++ frontend/app/src/components/EditUserProfile.vue
@@ -4,7 +4,7 @@
     <br>
     <section class="names">
 	<v-dialog v-model="dialog">
-    	<h1 class="username">{{user.userName}}</h1>
+		<h1 class="username">{{user.userName}}</h1>
 	</v-dialog>
       <h3>{{user.firstName}} {{user.lastName}}</h3>
     </section>
