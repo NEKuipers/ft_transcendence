e23485dde330081e965ad32ddad9ecf81857c371
pajotg <jasper.r.simonis@hotmail.nl>
2022-06-14 18:16:02 +0200

    SUPER SIMPLE match making added, pong only starts if it can find another player to play against, no interaction beyond that yet, Start 2 pages to test pong

--- socketio/src/app.ts
+++ socketio/src/app.ts
@@ -1,5 +1,5 @@
 import { createServer } from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
@@ -10,8 +10,62 @@ const io = new Server(httpServer, {
 	allowEIO3: true
 });
 
+/*
+io.use((socket, next) => {
+	console.log("handshake: ", socket.handshake)
+	return next();
+})
+*/
+
+class match {
+	p1: Socket;
+	p2: Socket;
+
+	constructor(p1: Socket, p2: Socket) {
+		this.p1 = p1;
+		this.p2 = p2;
+
+		console.log("Playing match between", p1.id, "and", p2.id)
+
+		p1.emit("match_start")
+		p2.emit("match_start")
+
+		p1.on("disconnect", (reason) => { console.log("match-disconnect! p1", reason); this.finish(); } );
+		p2.on("disconnect", (reason) => { console.log("match-disconnect! p2", reason); this.finish(); } );
+	}
+
+	finish() {
+		if (this.p1.connected) {
+			this.p1.emit("match_stop");
+			this.p1.off("disconnect", this.finish);
+			add_to_waiting_list(this.p1);
+		}
+		if (this.p2.connected) {
+			this.p2.emit("match_stop");
+			this.p2.off("disconnect", this.finish);
+			add_to_waiting_list(this.p2);
+		}
+	}
+}
+
+let waiting_for_game_connections : Array<Socket> = new Array();
+function matchmake() {
+	while (waiting_for_game_connections.length >= 2) {
+		let p1 = waiting_for_game_connections.pop();
+		let p2 = waiting_for_game_connections.pop();
+		
+		new match(p1, p2);
+	}
+}
+function add_to_waiting_list(socket: Socket) {
+	waiting_for_game_connections.push(socket);
+	matchmake();
+}
+
 io.on("connection", (socket) => {
-	console.log("Got connection:", socket.id)
+	console.log("Got connection:", socket.id);
+
+	add_to_waiting_list(socket);
 	
 	socket.emit("customEmit", "Hello from socketio server!");
 
@@ -23,6 +77,10 @@ io.on("connection", (socket) => {
 
 	socket.on("disconnect", (reason) => {
 		console.log("disconnected: ", socket.id, reason);
+		let index = waiting_for_game_connections.indexOf(socket);
+		if (index >= 0) {
+			waiting_for_game_connections.slice(index, 1);
+		}
 	})
 });
 
