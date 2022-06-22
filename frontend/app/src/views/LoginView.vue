9ad666d2482d39bae05a54720986c8e69e210093
Turlough Mullan <duke1199sp@gmail.com>
2022-06-22 18:05:46 +0200

    Exploring avenue for OAuth in login page

--- frontend/app/src/views/LoginView.vue
+++ frontend/app/src/views/LoginView.vue
@@ -3,7 +3,7 @@
     <h1>You have to login</h1>
     <img alt="pingpong" src="../assets/logo.png">
     <hr>
-    <button>Ask Jesse what to put here for OAuth</button>
+    <button @click="OAuthLogin">Login to Intra</button>
   </div>
 </template>
 
@@ -14,5 +14,20 @@ export default defineComponent({
   name: 'HomeView',
   components: {
   },
+  methods: {
+    OAuthLogin() {
+      console.log("Attempt to login")
+      // fetch("https://api.intra.42.fr/oauth/authorize", {
+      //   method: 'GET',
+      //   body: JSON.stringify({
+      //     'client_id': 1323232,
+      //     'redirect_uri': 'http://localhost:8080/',
+      //     'scope': 'public',
+      //     'state': 'true',
+      //     'response_type': 'code'
+      //   })
+      // })
+    }
+  }
 });
 </script>
