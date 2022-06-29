12eecc70d4c5b746bdb5259e28a4f5516ae13c97
tstelfox <tmullan@student.codam.nl>
2022-06-29 17:29:29 +0200

    Getting a 500 internal server error for some reason, once authentication has been processed by intra

--- backend/app/src/login/login.controller.ts
+++ backend/app/src/login/login.controller.ts
@@ -8,6 +8,8 @@ import { PassportStrategy } from '@nestjs/passport';
 import { passport } from 'passport-42'
 import { stringify } from 'querystring';
 import { IntraAuthGuard } from './guards';
+import { User } from '../users/user.interface'
+import { UsersService } from 'src/users/users.service';
 
 @Controller('login')
 @UseGuards(IntraAuthGuard)
@@ -18,35 +20,37 @@ export class LoginController {
         This is the route for intra authentication
     */
     @Get()
-    @Redirect()
     OAuthRequest(): any {
+        return;
     // OAuthRequest() : Observable<AxiosResponse<any, any>> {
-        var FortyTwoStrategy = require('passport-42').Strategy
-        // var passport = require('passport')
+        // var FortyTwoStrategy = require('passport-42').Strategy
+        // // var passport = require('passport')
 
-        console.log('Buongiorno diobestia')
-        passport.use(new FortyTwoStrategy({
-            clientID: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: 'http://127.0.0.1:3000/callback',
-            profileFields: {
-                'id': function (obj) { return stringify(obj.id)},
-                'username': 'login'
-            }
-          }))
-        // function(accessToken, refreshToken, profile, cb) {
-        //     return cb(null, profile)
-        // }))
+        // console.log('Buongiorno diobestia')
+        // passport.use(new FortyTwoStrategy({
+        //     clientID: process.env.CLIENT_ID,
+        //     clientSecret: process.env.CLIENT_SECRET,
+        //     callbackURL: 'http://127.0.0.1:3030/login/callback',
+        //     profileFields: {
+        //         'id': function (obj) { return stringify(obj.id)},
+        //         'username': 'login'
+        //     }
+        //   }))
+        // // function(accessToken, refreshToken, profile, cb) {
+        // //     return cb(null, profile)
+        // // }))
 
-        passport.authenticate('42')
+        // passport.authenticate('42')
     }
 
     /* 
         This is the redirect URL the OAuth2 Provider will call
     */
-    @Get(':callback')
+    @Get('callback')
+    @UseGuards(IntraAuthGuard)
     callback(@Res() res: Response) {
 
+        console.log('Yoo hello?')
         res.send(200)
         // var passport = require('passport')
 
