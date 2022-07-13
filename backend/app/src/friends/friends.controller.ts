67902b35047e2fc084528f3163bea8fafae2151f
Nick Kuipers <nickkuipers92@gmail.com>
2022-07-13 16:03:25 +0200

    Feat: added backend controllers for friends

--- backend/app/src/friends/friends.controller.ts
+++ backend/app/src/friends/friends.controller.ts
@@ -5,4 +5,25 @@ import { Friend } from './friends.interface';
 @Controller('friends')
 export class FriendsController {
 	constructor(private readonly friendsService: FriendsService) {}
+
+	@Get()
+	findAll(): Friend[] {
+		return this.friendsService.findAll();
+	}
+
+	@Post()
+	create(@Body() friend: Friend): string {
+		return this.friendsService.createFriendRequest(friend);
+	}
+
+	@Patch(':id')
+	update(@Param('id') id: number, @Body() friend: Friend) : string {
+		return this.friendsService.updateFriendRequest(id, friend);
+	}
+
+	@Delete(':id')
+	unfriend(@Param('id') id: number): string {
+		return this.friendsService.deleteFriend(id);
+	}
+
 }
--- backend/app/src/friends/friends.service.ts
+++ backend/app/src/friends/friends.service.ts
@@ -2,4 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { Friend } from './friends.interface';
 
 @Injectable()
-export class FriendsService {}
+export class FriendsService {
+	friends: Friend[] = [];
+
+	findAll(): Friend[] {
+		return this.friends;
+	}
+
+	createFriendRequest(friend: Friend): string {
+		//add friend to db via post request here
+		return "Friend request added to database";
+	}
+
+	updateFriendRequest(id: number, friend: Friend) : string { 
+		//update friend request when person who received the request responds
+		return "Friend request updated";
+	}
+
+	deleteFriend(id: number) : string {
+		//remove friend with id of number from friend table
+		return "Friend deleted"
+	}
+
+}
