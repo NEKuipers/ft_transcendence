c82f5586403f779af0b6970bc6918ddb8d1fc117
Nick Kuipers <nickkuipers92@gmail.com>
2022-07-21 17:26:33 +0200

    Blocked users now cannot see any information from others, nor can they access the buttons for inviting to game, dm'ing, etc

--- backend/app/src/blocked_users/blocked_users.service.ts
+++ backend/app/src/blocked_users/blocked_users.service.ts
@@ -23,6 +23,23 @@ export class BlockedUsersService {
 		});
 	}
 
+	findAllWhoBlockedUser(id: number) : Promise<BlockedUser[]> {
+		return new Promise((accept, reject) => {
+			axios.get(`http://localhost:${process.env.PGREST_PORT}/blocked_users?blocked_user_id=eq.${id}`)
+				.then((response) => {
+					if (response.status != 200) {
+						console.log(`Got statusCode: ${response.status} (${response.statusText}): ${JSON.stringify(response.headers, null, 4)}`)
+						reject(response);
+						return;
+					}
+					accept(response.data);
+				}).catch((error) => {
+					console.log(`Got error: ${error}`)
+					reject(error);
+				});
+		});
+	}
+
 	blockUser(blockedUser: BlockedUser) : string {
 		axios.post(`http://localhost:${process.env.PGREST_PORT}/blocked_users`, {
 			"blocked_by_id": blockedUser.blocked_by_id,
