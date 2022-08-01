8b3e9da254d674aaa63d69f630128184ff0eedb7
tstelfox <tmullan@student.codam.nl>
2022-08-01 18:14:52 +0200

    AM not having much luck with this

--- backend/app/src/avatars/avatars.service.ts
+++ backend/app/src/avatars/avatars.service.ts
@@ -34,17 +34,19 @@ export class AvatarsService {
 		console.log('Service can see the file:', file)
 
 		const buff = file.buffer
-		const fuck = buff.toString('hex')
+		const fuck = buff.toString('base64')
+		// .replace(/[\u0000]/g, "")
 
-		const byteArray = new Buffer(fuck.replace(/^[\w\d;:\/]+hex\,/g, ''), 'base64')
-		
-		// console.log(fuck)
+		// const byteArray = Buffer.from("\\x" + Buffer.from(fuck, "base64").toString("hex"))
+
+		// let dioporco = Uint8Array.from(buff)
+
 		// fuck.replace(/\\u([0-9]|[a-fA-F])([0-9]|[a-fA-F])([0-9]|[a-fA-F])([0-9]|[a-fA-F])/g, "")
 
 		const response = await axios.post(
 			`http://localhost:${process.env.PGREST_PORT}/avatars`,
 			{
-				img: byteArray,
+				img: fuck,
 				name: file.originalname,
 				// format: file.mimetype
 				format: 'img/png'
