f8ad56bf94c7c206a525dce713a7d3bb532edebf
tstelfox <tmullan@student.codam.nl>
2022-08-02 14:27:34 +0200

    Image is now posted correctlyyyyy. Now just need to make the patch request to the user with the correct avatar_id

--- backend/app/src/avatars/avatars.service.ts
+++ backend/app/src/avatars/avatars.service.ts
@@ -34,19 +34,15 @@ export class AvatarsService {
 		console.log('Service can see the file:', file)
 
 		const buff = file.buffer
-		const fuck = buff.toString('base64')
-		// .replace(/[\u0000]/g, "")
 
-		// const byteArray = Buffer.from("\\x" + Buffer.from(fuck, "base64").toString("hex"))
 
-		// let dioporco = Uint8Array.from(buff)
+		const imgdata = "\\x" + buff.toString("hex")
 
-		// fuck.replace(/\\u([0-9]|[a-fA-F])([0-9]|[a-fA-F])([0-9]|[a-fA-F])([0-9]|[a-fA-F])/g, "")
 
 		const response = await axios.post(
 			`http://localhost:${process.env.PGREST_PORT}/avatars`,
 			{
-				img: fuck,
+				img:  imgdata,
 				name: file.originalname,
 				// format: file.mimetype
 				format: 'img/png'
