531fdf2e2412e11a2b8a659823108435ede1630d
Nick Kuipers <nickkuipers92@gmail.com>
2022-08-17 20:56:01 +0200

    fix to create channel btn

--- frontend/app/src/components/MyChatChannels.vue
+++ frontend/app/src/components/MyChatChannels.vue
@@ -4,30 +4,27 @@
 			<div id="title">
 				<h5>Your Channels</h5>
 			</div>
-			<!-- <div>
-				<SmallButton text="Create new channel" @click="createChannel"/>
-				<DialogueBox :type="boxType" :show="showDialogue" @close-dialogue="hideDialogue" @new-name="saveChannel"/>
-			</div> -->
 			<div v-if="!myChannels">
 				<h5>Channels failed to load</h5>
 			</div>
-			<div v-else-if="myChannels?.length">
-				<div id="channels">
-					<SmallButton text="Create new channel" @click="createChannel"/>
-					<DialogueBox :type="boxType" :show="showDialogue" @close-dialogue="hideDialogue" @new-name="saveChannel"/>
-					<ul class="listed-channel" v-for="channel in myChannels" :key="channel.id">
-						<h5 class="name">{{ channel[0].name }}</h5>
-						<SmallButton  class="button" text="open" @click="openChat(channel[0].id)"/>
-						<SmallButton  class="button" text="leave" @click="leaveChannel(channel[0].id)"/>
-					</ul>
-				</div>
-			</div>
 			<div v-else>
-				<h5>You are not in any channels yet</h5>
+				<div v-if="myChannels?.length">
+					<div id="channels">
+						<ul class="listed-channel" v-for="channel in myChannels" :key="channel.id">
+							<h5 class="name">{{ channel[0].name }}</h5>
+							<SmallButton  class="button" text="open" @click="openChat(channel[0].id)"/>
+							<SmallButton  class="button" text="leave" @click="leaveChannel(channel[0].id)"/>
+						</ul>
+					</div>
+				</div>
+				<div v-else>
+					<h5>You are not in any channels yet</h5>
+				</div>
+				<SmallButton text="Create new channel" @click="createChannel"/>
+				<DialogueBox :type="boxType" :show="showDialogue" @close-dialogue="hideDialogue" @new-name="saveChannel"/>
 			</div>
 		</div>
-		<!-- <SmallButton text="Create new channel" @click="createChannel"/> -->
-		<!-- <DialogueBox :type="boxType" :show="showDialogue" @close-dialogue="hideDialogue" @new-name="saveChannel"/> -->
+
 	</div>
 </template>
 
