58c1d3523582888a8f76025a5d111fb4df3cecab
jesse.van.der.wolf <jesse.van.der.wolf@linkit.nl>
2022-09-08 21:56:52 +0200

    changes discussed during meeting at 21:15

--- database/postgres/sql/01_create_db_tables.sql
+++ database/postgres/sql/01_create_db_tables.sql
@@ -102,6 +102,7 @@ CREATE TABLE public.users
     oauth_refresh_token             VARCHAR(1024),
     oauth_token_expiration_time     TIMESTAMP,
     is_logged_in                    BOOL,
+    is_ingame                       BOOL,
     CONSTRAINT unique_username UNIQUE (username)
 );
 
@@ -170,7 +171,7 @@ CREATE TABLE public.participants
     participant_id  BIGINT REFERENCES public.users(id),
 	is_joined		BOOL,
     is_admin        BOOL,
-    is_muted        VARCHAR(256),
+    is_muted        TIMESTAMP,
     is_banned       BOOL,
     channel_id      BIGINT REFERENCES public.channels(id)
 );
